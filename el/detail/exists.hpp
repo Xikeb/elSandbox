681bd6c772901373510039f776b49f7ce4fdc8cb
Xikeb <elliot.kougbenya@epitech.eu>
2018-05-24 11:20:54 +0200

    Changed implementation of el::detail::exists for some tests on is_valid

--- el/detail/exists.hpp
+++ el/detail/exists.hpp
@@ -3,9 +3,19 @@
 	#include "el/types/integral_c.hpp"
 
 	namespace el {
+		namespace impl {
+			template<typename T>
+			constexpr int exists(int = 0) noexcept;
+
+			template<typename T>
+			constexpr void exists(...);
+		} // impl
 		namespace detail {
-			template<typename ...Types>
-			using exists = el::bool_c<sizeof...(Types) != 0>;
+			// template<typename ...Types>
+			// using exists = el::bool_c<sizeof...(Types) != 0>;
+
+			template<typename T>
+			using exists = el::bool_c<sizeof(el::impl::exists<T>()) != 0>;
 		} // detail
 	} // el
 #endif // ELMETA_EXISTS_HPP
