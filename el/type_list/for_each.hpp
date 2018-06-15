b88f0b58aa2475bba363bde65d608b00ec454fe1
Xikeb <elliot.kougbenya@epitech.eu>
2018-06-16 00:36:42 +0200

    type_list::for_each now returns std::move(callback), as does the STL

--- el/type_list/for_each.hpp
+++ el/type_list/for_each.hpp
@@ -1,25 +1,27 @@
 #ifndef ELMETA_FOR_EACH_HPP
 	#define ELMETA_FOR_EACH_HPP
+	#include <utility>
 
 	namespace el {
 		namespace impl {
 			template<typename TList, typename TF, typename ...Args>
-			void for_each(el::size_c<(TList::size - 1)> i, TF&& f, Args&&... args)
+			auto for_each(el::size_c<(TList::size - 1)> i, TF&& f, Args&&... args)
 			{
 				f(
 					el::type_c<typename TList::template At<(TList::size - 1)>>,
 					i, std::forward<Args>(args)...
 				);
+				return std::move(f);
 			}
 
 			template<typename TList, std::size_t Idx, typename TF, typename ...Args>
-			void for_each(el::size_c<Idx> i, TF&& f, Args&&... args)
+			auto for_each(el::size_c<Idx> i, TF&& f, Args&&... args)
 			{
 				f(
 					el::type_c<typename TList::template At<Idx>>,
 					i, std::forward<Args>(args)...
 				);
-				for_each<TList>(
+				return for_each<TList>(
 					el::size_c<Idx + 1>(),
 					std::forward<TF>(f),
 					std::forward<Args>(args)...
