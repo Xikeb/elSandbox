9743ad3de0280150c53fc524ed93f04dfd599d12
Xikeb <elliot.kougbenya@epitech.eu>
2018-12-12 17:38:19 +0100

    pragma once

--- ecs/include/Entity.hpp
+++ ecs/include/Entity.hpp
@@ -5,102 +5,100 @@
  *      Author: eliord
  */
 
-#ifndef ENTITY_H_
-	#define ENTITY_H_
-	#include <cstdlib>
-	#include "Signature.hpp"
+#pragma once
+#include <cstdlib>
+#include "Signature.hpp"
 
-	namespace ecs {
-		using EntityIdx = std::size_t;
-		using HandleDataIdx = std::size_t;
+namespace ecs {
+	using EntityIdx = std::size_t;
+	using HandleDataIdx = std::size_t;
 
-		template<typename TSettings>
-		class Entity {
-		public:
-			using Settings = TSettings;
+	template<typename TSettings>
+	class Entity {
+	public:
+		using Settings = TSettings;
 
-			Entity(ecs::EntityIdx eid, HandleDataIdx hdIndex):
-			eid(eid), hdIndex(hdIndex), _alive(true)
-			{
-				this->_signature.reset();
-			}
+		Entity(ecs::EntityIdx eid, HandleDataIdx hdIndex):
+		eid(eid), hdIndex(hdIndex), _alive(true)
+		{
+			this->_signature.reset();
+		}
 
-			Entity(Entity const &oth):
-			eid(oth.eid), hdIndex(oth.hdIndex),
-			_alive(oth._alive), _signature(oth._signature)
-			{
-			}
+		Entity(Entity const &oth):
+		eid(oth.eid), hdIndex(oth.hdIndex),
+		_alive(oth._alive), _signature(oth._signature)
+		{
+		}
 
-			template<typename TSignature>
-			bool matchesSignature(TSignature&& sig = TSignature())
-			{
-				return sig.compare(this->_signature);
-			}
+		template<typename TSignature>
+		bool matchesSignature(TSignature&& sig = TSignature())
+		{
+			return sig.compare(this->_signature);
+		}
 
-			bool alive() const noexcept
-			{
-				return this->_alive;
-			}
+		bool alive() const noexcept
+		{
+			return this->_alive;
+		}
 
-			void kill() noexcept
-			{
-				this->_alive = false;
-			}
+		void kill() noexcept
+		{
+			this->_alive = false;
+		}
 
-			Entity &operator=(Entity const &rhs)
-			{
-				this->_alive = rhs._alive;
-				this->eid = rhs.eid;
-				this->hdIndex = rhs.hdIndex;
-				this->_signature = rhs._signature;
-				return *this;
-			}
+		Entity &operator=(Entity const &rhs)
+		{
+			this->_alive = rhs._alive;
+			this->eid = rhs.eid;
+			this->hdIndex = rhs.hdIndex;
+			this->_signature = rhs._signature;
+			return *this;
+		}
 
-			template<typename T>
-			bool hasComponent() const noexcept
-			{
-				return this->_signature.template hasComponent<T>();
-			}
+		template<typename T>
+		bool hasComponent() const noexcept
+		{
+			return this->_signature.template hasComponent<T>();
+		}
 
-			template<typename T>
-			void addComponent() noexcept
-			{
-				this->_signature.template enableComponent<T>();
-			}
+		template<typename T>
+		void addComponent() noexcept
+		{
+			this->_signature.template enableComponent<T>();
+		}
 
-			template<typename T>
-			void removeComponent() noexcept
-			{
-				this->_signature.template disableComponent<T>();
-			}
+		template<typename T>
+		void removeComponent() noexcept
+		{
+			this->_signature.template disableComponent<T>();
+		}
 
-			template<typename T>
-			bool hasTag() const noexcept
-			{
-				return this->_signature.template hasTag<T>();
-			}
+		template<typename T>
+		bool hasTag() const noexcept
+		{
+			return this->_signature.template hasTag<T>();
+		}
 
-			template<typename T>
-			void addTag() noexcept
-			{
-				this->_signature.template enableTag<T>();
-			}
+		template<typename T>
+		void addTag() noexcept
+		{
+			this->_signature.template enableTag<T>();
+		}
 
-			template<typename T>
-			void removeTag() noexcept
-			{
-				this->_signature.template disableTag<T>();
-			}
+		template<typename T>
+		void removeTag() noexcept
+		{
+			this->_signature.template disableTag<T>();
+		}
 
-			operator bool() const noexcept { return this->alive(); }
-			bool operator!() const noexcept { return !this->alive(); }
+		operator bool() const noexcept { return this->alive(); }
+		bool operator!() const noexcept { return !this->alive(); }
 
-			ecs::EntityIdx eid;
-			ecs::HandleDataIdx hdIndex;
+		ecs::EntityIdx eid;
+		ecs::HandleDataIdx hdIndex;
 
-		private:
-			bool _alive;
-			SignatureBitset<typename Settings::Basic> _signature;
-		};
-	} // ecs
-#endif /* ENTITY_H_ */
+	private:
+		bool _alive;
+		SignatureBitset<typename Settings::Basic> _signature;
+	};
+} // ecs
